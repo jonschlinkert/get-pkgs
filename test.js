b7bf32740e2ab427ebbc8d5b38ff067e6d5af7bd
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-10 18:59:38 -0400

    run update

--- test.js
+++ test.js
@@ -12,22 +12,22 @@ require('mocha');
 var assert = require('assert');
 var get = require('./');
 
-describe('getPackages', function () {
-  it('should get the package.json for the given repo', function (cb) {
+describe('getPackages', function() {
+  it('should get the package.json for the given repo', function(cb) {
     get('generate', function(err, pkgs) {
       pkgs[0].should.have.property('name', 'generate');
       cb();
     });
   });
 
-  it('should not error on non-existing repos', function (cb) {
+  it('should not error on non-existing repos', function(cb) {
     get('fofofof', function(err, pkgs) {
       assert(pkgs.length === 0);
       cb();
     });
   });
 
-  it('should get package.json files for an array of repos', function (cb) {
+  it('should get package.json files for an array of repos', function(cb) {
     get(['assemble', 'verb'], function(err, pkgs) {
       pkgs[0].should.have.property('name', 'assemble');
       pkgs[1].should.have.property('name', 'verb');
